118700d07150801f34c7f25a9eb9cd4a4e816417
Mikhail Istomin <mikhail_istomin@epam.com>
2024-01-31 16:52:41 +0400

    test chados for displacement textures

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdedede);
+
 
 const camera = new THREE.PerspectiveCamera( 
   75, window.innerWidth / window.innerHeight, 0.1, 2000 );
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 document.body.appendChild( renderer.domElement );
 
 const axesHelper = new THREE.AxesHelper(1000)
 scene.add(axesHelper)
 
-const light = new THREE.PointLight(0xffffff, 1000)
-light.position.set(100, 100, 400)
-scene.add(light)
 
 // box
 const boxTexture = new THREE.TextureLoader().load('/crate.gif');
@@ -30,7 +32,7 @@ const boxMaterial = new THREE.MeshBasicMaterial( {
   map: boxTexture 
 } );
 const box = new THREE.Mesh( boxGeometry, boxMaterial );
-box.position.set(-300,300,0)
+box.position.set(-300,300,-100)
 const edgesGeometry = new THREE.EdgesGeometry( boxGeometry );
 const edgeColor = new THREE.Color(0x000000)
 const edgesMaterial = new THREE.LineBasicMaterial( { color: edgeColor}); 
@@ -99,13 +101,53 @@ const box3Material = new THREE.MeshStandardMaterial( {
   displacementBias: -25
 } );
 const box3 = new THREE.Mesh( box3Geometry, box3Material );
-box3.position.set(250,250,0)
+box3.position.set(-660,250,0);
+box3.castShadow = true;
+box3.receiveShadow = true;
 scene.add( box3 );
+
+const floorGeo = new THREE.PlaneGeometry(1000, 1000, 100, 100);
+const floor = new THREE.Mesh(
+  floorGeo,
+  new THREE.MeshStandardMaterial( { 
+    color: 0xaaaaaa,
+  }),
+);
+floor.receiveShadow = true;
+floor.castShadow = true;
+scene.add( floor );
+
+const sphereGeometry = new THREE.SphereGeometry( 15, 32, 32 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.position.set(0,0,130)
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
+scene.add( sphere );
+
+const light = new THREE.PointLight(0xffffff, 1000000, 100000)
+light.position.set(-550, 178, 399);
+light.castShadow = true;
+light.receiveShadow = true;
+light.shadow.mapSize.width = 512; // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.5; // default
+light.shadow.camera.far = 1000; // default
+
+scene.add(light)
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 camera.position.set(800, 700, 500);
 camera.lookAt(300,300,0);
+const helper = new THREE.CameraHelper( light.shadow.camera );
+scene.add( helper );
+const gui = new GUI();
+const lightFolder = gui.addFolder('Light');
+lightFolder.add(light.position, 'x').min(-1000).max(1000);
+lightFolder.add(light.position, 'y').min(-1000).max(1000)
+lightFolder.add(light.position, 'z').min(-1000).max(1000)
+lightFolder.open();
 
 function animate() {
 	requestAnimationFrame( animate );
